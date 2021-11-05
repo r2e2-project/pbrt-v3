b29c64be4b30048f727ff42da527b79c965b75d5
Sadjad Fouladi <sfouladi@gmail.com>
2021-11-05 10:24:10 -0700

    print-manifest.cpp: Print texture/material information for treelets.

--- src/cloud/print-manifest.cpp
+++ src/cloud/print-manifest.cpp
@@ -3,6 +3,7 @@
 
 #include "cloud/manager.h"
 #include "include/pbrt/common.h"
+#include "messages/lite.h"
 #include "messages/utils.h"
 #include "util/exception.h"
 #include "util/path.h"
@@ -22,7 +23,8 @@ void print_deps(const protobuf::Manifest& manifest, const ObjectKey& root,
             for (const auto& dep_proto : obj_proto.dependencies()) {
                 auto dep = from_protobuf(dep_proto);
                 cout << string(level * 2, ' ') << "\u21b3 "
-                     << _manager.getFileName(dep.type, dep.id) << "  \033[38;5;242m"
+                     << _manager.getFileName(dep.type, dep.id)
+                     << "  \033[38;5;242m"
                      << format_bytes(roost::file_size(
                             _manager.getFilePath(dep.type, dep.id)))
                      << "\033[0m" << endl;
@@ -30,6 +32,85 @@ void print_deps(const protobuf::Manifest& manifest, const ObjectKey& root,
             }
         }
     }
+}
+
+void print_treelet_info(const uint32_t treelet_id) {
+    vector<char> treelet_buffer;
+
+    const string treelet_path =
+        _manager.getScenePath() + "/" +
+        _manager.getFileName(ObjectType::Treelet, treelet_id);
+
+    ifstream fin{treelet_path, ios::binary | ios::ate};
+
+    if (!fin.good()) {
+        throw runtime_error("Could not open treelet file: " + treelet_path);
+    }
+
+    streamsize size = fin.tellg();
+    fin.seekg(0, ios::beg);
+
+    treelet_buffer.resize(size);
+    fin.read(treelet_buffer.data(), size);
+
+    LiteRecordReader reader{treelet_buffer.data(), treelet_buffer.size()};
+
+    size_t total_texture_size = 0;
+    uint32_t included_texture_count;
+    reader.read(&included_texture_count);
+    for (size_t i = 0; i < included_texture_count; i++) {
+        uint32_t id;
+        reader.read(&id);
+        size_t l;
+        reader.read(nullptr, &l);
+        total_texture_size += l;
+    }
+
+    size_t total_spectrum_size = 0;
+    uint32_t included_spectrum_count = 0;
+    reader.read(&included_spectrum_count);
+    for (size_t i = 0; i < included_spectrum_count; i++) {
+        uint32_t id;
+        reader.read(&id);
+        size_t l;
+        reader.read(nullptr, &l);
+        total_spectrum_size += l;
+    }
+
+    size_t total_float_size = 0;
+    uint32_t included_float_count = 0;
+    reader.read(&included_float_count);
+    for (size_t i = 0; i < included_float_count; i++) {
+        uint32_t id;
+        reader.read(&id);
+        size_t l;
+        reader.read(nullptr, &l);
+        total_float_size += l;
+    }
+
+    size_t total_material_size = 0;
+    uint32_t included_material_count = 0;
+    reader.read(&included_material_count);
+    for (size_t i = 0; i < included_material_count; i++) {
+        uint32_t id;
+        reader.read(&id);
+        size_t l;
+        reader.read(nullptr, &l);
+        total_material_size += l;
+    }
+
+    uint32_t included_mesh_count = 0;
+    reader.read(&included_mesh_count);
+
+    cout << "\u21b3 TEX:  " << included_texture_count << "  \033[38;5;242m"
+         << format_bytes(total_texture_size) << "\033[0m" << endl
+         << "\u21b3 STEX: " << included_spectrum_count << "  \033[38;5;242m"
+         << format_bytes(total_spectrum_size) << "\033[0m" << endl
+         << "\u21b3 FTEX: " << included_float_count << "  \033[38;5;242m"
+         << format_bytes(total_float_size) << "\033[0m" << endl
+         << "\u21b3 MAT:  " << included_material_count << "  \033[38;5;242m"
+         << format_bytes(total_material_size) << "\033[0m" << endl
+         << "\u21b3 MESH: " << included_mesh_count << endl;
 }
 
 int main(int argc, char* argv[]) {
@@ -54,6 +135,7 @@ int main(int argc, char* argv[]) {
                  << format_bytes(roost::file_size(
                         _manager.getFilePath(ObjectType::Treelet, i)))
                  << ")" << endl;
+            print_treelet_info(i);
             print_deps(manifest_proto, {ObjectType::Treelet, i}, 0);
         }
     } catch (const exception& e) {
